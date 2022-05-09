4ce4ffb942a6c6310803898adb97a407b07431f1
Kese04 <kese041@gmail.com>
2022-05-09 10:33:13 -0400

    Zero-ed signals, removed fcns, added rising edge
    1) Found an infinite loop with PC while testing. Since the enable wasn't set to trigger on the rising edge, it keep looping as long as the enable was 1. I've modifed all the modules with an enable to trigger on a rising edge now.
    2) Removed the read from text fcns in the REG and RAM files (Only one that has it now is ROM, for the Fibonacci). Also initialized all the modules to start at zero (for both the inputs, outputs, and signals, except for ROM's `ROM` signal)

--- Lab6Modules.vhd
+++ Lab6Modules.vhd
@@ -4,50 +4,23 @@ use IEEE.NUMERIC_STD.ALL;
 use std.textio.all;
 
 entity RAM is
-    Port ( I_RAM_EN : in  STD_LOGIC;
-           I_RAM_RE : in  STD_LOGIC;
-           I_RAM_WE : in  STD_LOGIC;
-           I_RAM_ADDR : in  STD_LOGIC_VECTOR (31 downto 0);
-           I_RAM_DATA : in  STD_LOGIC_VECTOR (31 downto 0);
-           O_RAM_DATA : out  STD_LOGIC_VECTOR (31 downto 0));
+    Port ( I_RAM_EN : in  STD_LOGIC := '0';
+           I_RAM_RE : in  STD_LOGIC := '0';
+           I_RAM_WE : in  STD_LOGIC := '0';
+           I_RAM_ADDR : in  STD_LOGIC_VECTOR (31 downto 0) := (others => '0');
+           I_RAM_DATA : in  STD_LOGIC_VECTOR (31 downto 0) := (others => '0');
+           O_RAM_DATA : out  STD_LOGIC_VECTOR (31 downto 0) := (others => '0'));
 end RAM;
 
 architecture Behavioral of RAM is
 	
 	type RAM256x8 is array(0 to 255) of std_logic_vector(7 downto 0);
-	
-	--function to load the contents of a file into RAM memory
-	--will read up to 64 lines of the file 
-	impure function init_buf(FileName: in string) return RAM256x8 is
-		constant LINE_NUM : integer := 64;
-		file fp : text;
-		variable temp_mem: RAM256x8 := (others => x"00");
-		variable line_cache : line;
-		variable byte_cache : bit_vector (31 downto 0) := x"00000000";
-	begin
-		file_open(fp, FileName, read_mode);
-		for i in 0 to LINE_NUM loop
-			if endfile(fp) then
-				exit;
-			else 
-				readline(fp, line_cache);
-				read(line_cache, byte_cache);
-				temp_mem(i*4) := to_stdlogicvector(byte_cache)(31 downto 24);
-				temp_mem((i*4)+1) := to_stdlogicvector(byte_cache)(23 downto 16);
-				temp_mem((i*4)+2) := to_stdlogicvector(byte_cache)(15 downto 8);
-				temp_mem((i*4)+3) := to_stdlogicvector(byte_cache)(7 downto 0);
-			end if;
-		end loop;
-		file_close(fp);
-		return temp_mem;
-	end function;
-	
-	-- init mem from the file 
-	signal mem : RAM256x8 := init_buf("RAM_init.txt");
+	signal mem : RAM256x8 := (others=>(others=>'0'));
 	
 begin	
 	process (I_RAM_EN, I_RAM_WE, I_RAM_RE, I_RAM_DATA, I_RAM_ADDR)
 	begin
+	if rising_edge(I_RAM_EN) then
 		-- enable = '1'
 		if (I_RAM_EN = '1') then 
 			-- write = '1'
@@ -63,6 +36,7 @@ begin
 				O_RAM_DATA <= mem(to_integer(unsigned(I_RAM_ADDR)) - 8192);
 			end if;
 		end if;
+	end if;
 	end process;
 end Behavioral;
 
@@ -73,47 +47,24 @@ use IEEE.NUMERIC_STD.ALL;
 use std.textio.all;
 
 entity REG is
-    Port ( I_REG_EN : in  STD_LOGIC;
-           I_REG_WE : in  STD_LOGIC;
-           I_REG_SEL_RS : in  STD_LOGIC_VECTOR (4 downto 0);
-           I_REG_SEL_RT : in  STD_LOGIC_VECTOR (4 downto 0);
-           I_REG_SEL_RD : in  STD_LOGIC_VECTOR (4 downto 0); --This is more of a "write register" than RD
-           I_REG_DATA_RD : in  STD_LOGIC_VECTOR (31 downto 0);
+    Port ( I_REG_EN : in  STD_LOGIC := '0';
+           I_REG_WE : in  STD_LOGIC := '0';
+           I_REG_SEL_RS : in  STD_LOGIC_VECTOR (4 downto 0) := (others => '0');
+           I_REG_SEL_RT : in  STD_LOGIC_VECTOR (4 downto 0) := (others => '0');
+           I_REG_SEL_RD : in  STD_LOGIC_VECTOR (4 downto 0) := (others => '0'); --This is more of a "write register" than RD
+           I_REG_DATA_RD : in  STD_LOGIC_VECTOR (31 downto 0) := (others => '0');
            O_REG_DATA_A : out  STD_LOGIC_VECTOR (31 downto 0) := (others => '0');
            O_REG_DATA_B : out  STD_LOGIC_VECTOR (31 downto 0) := (others => '0'));
 end REG;
 
 architecture Behavioral of REG is
 	type REG_FILE is array (0 to 31) of STD_LOGIC_VECTOR (31 downto 0);
-	
-	--Function to pull 32 32-bit register values from a text file,
-	-- and return them as a REG_FILE
-	impure function init_reg_file(FileName: in string) return REG_FILE is
-		constant LINE_NUM : integer := 32;
-		file fp : text;
-		variable temp_REG_FILE: REG_FILE := (others => x"00000000");
-		variable line_cache : line;
-		variable word_cache : bit_vector (31 downto 0) := x"00000000";
-	begin
-		file_open(fp, FileName, read_mode);
-		for i in 0 to LINE_NUM loop
-			if endfile(fp) then
-				exit;
-			else
-				readline(fp, line_cache);
-				read(line_cache, word_cache);
-				temp_REG_FILE(i) := to_stdlogicvector(word_cache);
-			end if;
-		end loop;
-		file_close(fp);
-		return temp_REG_FILE;
-	end function;
-	
-	signal Registers : REG_FILE := init_reg_file("REG_init.txt");
+	signal Registers : REG_FILE := (others=>(others=>'0'));
 begin
 	process(I_REG_EN, I_REG_WE, I_REG_SEL_RS, I_REG_SEL_RT, I_REG_SEL_RD, I_REG_DATA_RD)
 	variable RS, RT, RD : integer := 0;
 	begin
+	if rising_edge(I_REG_EN) then
 		if(I_REG_EN = '1') then
 			RS := to_integer(unsigned(I_REG_SEL_RS));
 			RT := to_integer(unsigned(I_REG_SEL_RT));
@@ -126,6 +77,7 @@ begin
 				end if;
 			end if;
 		end if;
+	end if;
 	end process;
 end Behavioral;
 
@@ -138,9 +90,9 @@ use UNISIM.VComponents.all;
 use STD.textio.all;
 
 entity ROM is
-    Port ( I_ROM_EN : in  STD_LOGIC;
-           I_ROM_ADDR : in  STD_LOGIC_VECTOR (31 downto 0);
-           O_ROM_DATA : out  STD_LOGIC_VECTOR (31 downto 0));
+    Port ( I_ROM_EN : in  STD_LOGIC := '0';
+           I_ROM_ADDR : in  STD_LOGIC_VECTOR (31 downto 0) := (others => '0');
+           O_ROM_DATA : out  STD_LOGIC_VECTOR (31 downto 0) := (others => '0'));
 end ROM;
 
 architecture Behavioral of ROM is
@@ -209,10 +161,12 @@ ROM <= initialize_ROM("Fibonacci.bin");
 	
 fetch_instruction: process (I_ROM_EN, I_ROM_ADDR)
 	begin
+	if rising_edge(I_ROM_EN) then
 	
 		if I_ROM_EN = '1' then 
 			O_ROM_DATA <= ROM(to_integer(unsigned(I_ROM_ADDR)));
 			
 		end if;
+	end if;
 	end process;
 end Behavioral;
